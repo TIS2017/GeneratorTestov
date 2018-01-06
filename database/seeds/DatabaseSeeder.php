2e37d46028e8fa503a9568c4530b0ecaa44661b7
Milan Cifra <cifra9@uniba.sk>
2018-01-06 19:26:14 +0100

    seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,57 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+
+        DB::table('questions')->delete();
+        DB::table('keywords')->delete();
+        DB::table('keywords_questions')->delete();
+
+        $q1 = \App\Question::create([
+            'question' => 'question 1',
+            'points' => 10,
+            'practical' => true
+        ]);
+
+        $q2 = \App\Question::create([
+            'question' => 'question 2',
+            'points' => 20,
+            'practical' => false
+        ]);
+
+        $q3 = \App\Question::create([
+            'question' => 'question 3',
+            'points' => 30,
+            'practical' => true
+        ]);
+
+        $this->command->info('Questions added!');
+
+        $k1 = \App\Keyword::create([
+            'keyword' => 'keyword 1'
+        ]);
+
+        $k2 = \App\Keyword::create([
+            'keyword' => 'keyword 2'
+        ]);
+
+        $k3 = \App\Keyword::create([
+            'keyword' => 'keyword 3'
+        ]);
+
+        $this->command->info('Keywords added!');
+
+        $q1->keywords()->attach($k1->id);
+        $q1->keywords()->attach($k2->id);
+        $q1->keywords()->attach($k3->id);
+
+        $q2->keywords()->attach($k3->id);
+
+        $q3->keywords()->attach($k1->id);
+        $q3->keywords()->attach($k2->id);
+
+        $k3->questions()->attach($q1->id);
+
+        $this->command->info('Relations added!');
+
     }
 }
