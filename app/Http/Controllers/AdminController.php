4ad8d5a2b03fa54c16dfc4d6a405ead6dbd72428
milancifra <cifra9@uniba.sk>
2018-01-09 18:25:18 +0100

    nacitanie obrazkov

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,13 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Question;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class AdminController extends Controller
 {
     public function listQuestions(Request $request)
     {
         $questions = Question::paginate(20);
-        return view('all_questions', ['questions' => $questions]);
+        $question_images = [];
+        foreach ($questions as $question) {
+            $images = Storage::files('public/question_images/' . $question->id);
+            $question_images[$question->id] = $images;
+        }
+        return view('all_questions', ['questions' => $questions, 'question_images' => $question_images]);
     }
 
     public function addQuestion(Request $request)
