414500d15d44f71c7012fc7bfdd37b0d2ee48385
milancifra <cifra9@uniba.sk>
2018-01-16 20:35:43 +0100

    view

---
+++ resources/views/all_questions.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/styly.css') }}" media="all">
+        <script src="{{ asset('/js/app.js') }}"></script>
+        <title>Generátor testov</title>
+    </head>
+    <body>
+        <div class="bodyStyle">
+            <header>
+                <h1>Generátor testov</h1>
+            </header>
+            <nav>
+                <a href="index.html">Domov</a>
+                <a href="pridajOtazku.html">Pridaj otázku</a>
+                <a href="generator.html">Generátor</a>
+                <a href="administracia.html">Administrácia</a>
+            </nav>
+            <section>
+                <p>Administrácia otázok</p>
+                <br>
+                @foreach($questions as $question)
+                    <div class="questionBlock">
+                        {{ $question->id }} - {{ $question->question }}
+                    </div>
+                    <div class="btn-group">
+                        <form action="pridajOtazku.html">
+                            <button>Uprav</button>
+                        </form>
+                        <button>Zmaž</button>
+                    </div>
+                    {{--@foreach($question_images[$question->id] as $image)--}}
+                        {{--<img src="{{ Storage::url($image) }}">--}}
+                    {{--@endforeach--}}
+                    {{--<ul>--}}
+                        {{--@foreach($question->keywords as $keyword)--}}
+                            {{--<li>{{ $keyword->keyword }}</li>--}}
+                        {{--@endforeach--}}
+                    {{--</ul>--}}
+                @endforeach
+                {{ $questions->links() }}
+            </section>
+            <footer>
+                &copy;Vytvoril tím RMC 2017
+            </footer>
+        </div>
+    </body>
+</html>
+
+
+
+
+
